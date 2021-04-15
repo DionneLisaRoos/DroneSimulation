68ca933f0cc91e9b5482c6f47b1bdb34d7f2323a
Dionne Ariens <dlr.ariens@outlook.com>
2021-04-15 15:53:20 +0200

    x initiate after x.clear with x0

--- DroneSimulator/DroneSimulator/SimStatic.h
+++ DroneSimulator/DroneSimulator/SimStatic.h
@@ -8,7 +8,7 @@ class SimStatic :
 {
 public:
 
-	SimStatic(std::vector<double> x0, InputStatic input, double timestep, bool Cargo) : graphics(Cargo)
+	SimStatic(std::vector<double> X0, InputStatic input, double timestep, bool Cargo) : graphics(Cargo), x0(X0)
 	{
 		x.push_back(x0);
 		cargo = Cargo;
@@ -23,6 +23,7 @@ public:
 	void staticSimulateFullEuler()
 	{
 		x.clear();
+		x.push_back(x0);
 		// 2 options, make if else for cargo to update graphics
 		// or pass full xk to graphics and discriminate between cargo or no in the grapgics class
 		if (cargo)
@@ -48,6 +49,7 @@ public:
 	void staticSimulateFullRungeKutta()
 	{
 		x.clear();
+		x.push_back(x0);
 		if (cargo)
 		{
 			for (size_t i = 1; i < t.size(); i++)
@@ -77,6 +79,7 @@ private:
 	std::vector<std::vector<double>> x;
 	std::vector<double> t;
 	std::vector<std::array<double, 2>> u;
+	std::vector<double> x0;
 	bool cargo;
 	Graphics graphics;
 };
