75eb3a666b42b11a655e71416b532fd797bd85f0
Dionne Ariens <dlr.ariens@outlook.com>
2021-04-16 01:41:07 +0200

    add GUI start

--- DroneSimulator/DroneSimulator/mouseclicks.cpp
+++ DroneSimulator/DroneSimulator/mouseclicks.cpp
@@ -6,10 +6,11 @@ and may not be redistributed without written permission.*/
 #include <SDL_image.h>
 #include <stdio.h>
 #include <string>
+#include <iostream>
 
 //Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH1 = 640;
+const int SCREEN_HEIGHT1 = 400;
 
 //Button constants
 const int BUTTON_WIDTH = 300;
@@ -25,6 +26,21 @@ enum LButtonSprite
 	BUTTON_SPRITE_TOTAL = 4
 };
 
+enum class Images
+{
+	IMAGE_DRONE = 0,
+	IMAGE_DRONE_AND_CARGO = 1,
+	IMAGE_TITLE = 2,
+	IMAGE_TEXT_DRONE = 3,
+	IMAGE_TEXT_DRONE_AND_CARGO = 4,
+	IMAGE_KEYBOARD = 5,
+	IMAGE_CSV_FILE = 6,
+	IMAGE_TEXT_KEYBOARD = 7,
+	IMAGE_TEXT_CSV_FILE = 8,
+	IMAGE_FILENAME = 9,
+	TOTAL_IMAGES = 10
+};
+
 //Texture wrapper class
 class LTexture
 {
@@ -38,7 +54,7 @@ public:
 	//Loads image at specified path
 	bool loadFromFile(std::string path);
 
-#if defined(SDL_TTF_MAJOR_VERSION)
+#if defined(SDL_TTF_MAJOR_VERSION)d
 	//Creates image from font string
 	bool loadFromRenderedText(std::string textureText, SDL_Color textColor);
 #endif
@@ -56,15 +72,17 @@ public:
 	void setAlpha(Uint8 alpha);
 
 	//Renders texture at given point
-	void render(int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
+	void renderr(int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
 
 	//Gets image dimensions
 	int getWidth();
 	int getHeight();
 
+	SDL_Texture* mTexture;
+
 private:
 	//The actual hardware texture
-	SDL_Texture* mTexture;
+	
 
 	//Image dimensions
 	int mWidth;
@@ -84,12 +102,16 @@ public:
 	//Handles mouse event
 	void handleEvent(SDL_Event* e);
 
+	bool isPushed() { return pushed; }
+	void clearPushedFlag() { pushed = false; }
+
 	//Shows button sprite
-	void render();
+	//void render();
 
 private:
 	//Top left position
 	SDL_Point mPosition;
+	bool pushed;
 
 	//Currently used global sprite
 	LButtonSprite mCurrentSprite;
@@ -111,11 +133,20 @@ SDL_Window* gWindow = NULL;
 SDL_Renderer* gRenderer = NULL;
 
 //Mouse button sprites
-SDL_Rect gSpriteClips[BUTTON_SPRITE_TOTAL];
-LTexture gButtonSpriteSheetTexture;
+SDL_Rect gImages[static_cast<int>(Images::TOTAL_IMAGES)];
+LTexture gDrone;
+LTexture gDroneWithCargo;
+LTexture gTitle;
+LTexture gtextDrone;
+LTexture gtextDroneWithCargo;
+LTexture gKeyboard;
+LTexture gCSVfile;
+LTexture gtextKeyboard;
+LTexture gtextCSVfile;
+LTexture gfileName;
 
 //Buttons objects
-LButton gButtons[TOTAL_BUTTONS];
+LButton gButtons[2];
 
 LTexture::LTexture()
 {
@@ -147,6 +178,7 @@ bool LTexture::loadFromFile(std::string path)
 	}
 	else
 	{
+		//TODO: uncommand, setcolorkey. 
 		//Color key image
 		SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
 
@@ -238,7 +270,7 @@ void LTexture::setAlpha(Uint8 alpha)
 	SDL_SetTextureAlphaMod(mTexture, alpha);
 }
 
-void LTexture::render(int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip)
+void LTexture::renderr(int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip)
 {
 	//Set rendering space and render to screen
 	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
@@ -268,6 +300,7 @@ LButton::LButton()
 {
 	mPosition.x = 0;
 	mPosition.y = 0;
+	pushed = false;
 
 	mCurrentSprite = BUTTON_SPRITE_MOUSE_OUT;
 }
@@ -281,7 +314,7 @@ void LButton::setPosition(int x, int y)
 void LButton::handleEvent(SDL_Event* e)
 {
 	//If mouse event happened
-	if (e->type == SDL_MOUSEMOTION || e->type == SDL_MOUSEBUTTONDOWN || e->type == SDL_MOUSEBUTTONUP)
+	if (e->type == SDL_MOUSEBUTTONDOWN)
 	{
 		//Get mouse position
 		int x, y;
@@ -314,13 +347,17 @@ void LButton::handleEvent(SDL_Event* e)
 		//Mouse is outside button
 		if (!inside)
 		{
-			mCurrentSprite = BUTTON_SPRITE_MOUSE_OUT;
+			//mCurrentSprite = BUTTON_SPRITE_MOUSE_OUT;
 		}
 		//Mouse is inside button
 		else
 		{
+			if (e->type == SDL_MOUSEBUTTONDOWN) {
+				//std::cout << "button pushed\n" << this->mCurrentSprite << std::endl;
+				this->pushed = true;
+			}
 			//Set mouse over sprite
-			switch (e->type)
+			/*switch (e->type)
 			{
 			case SDL_MOUSEMOTION:
 				mCurrentSprite = BUTTON_SPRITE_MOUSE_OVER_MOTION;
@@ -333,15 +370,38 @@ void LButton::handleEvent(SDL_Event* e)
 			case SDL_MOUSEBUTTONUP:
 				mCurrentSprite = BUTTON_SPRITE_MOUSE_UP;
 				break;
-			}
+			}*/
 		}
 	}
 }
 
-void LButton::render()
+void render(bool choice)
 {
-	//Show current button sprite
-	gButtonSpriteSheetTexture.render(mPosition.x, mPosition.y, &gSpriteClips[mCurrentSprite]);
+	if (!choice) {
+		SDL_RenderCopyEx(gRenderer, gDrone.mTexture, NULL, &gImages[static_cast<int>(Images::IMAGE_DRONE)], 0.0, NULL, SDL_FLIP_NONE);
+		SDL_RenderCopyEx(gRenderer, gDroneWithCargo.mTexture, NULL, &gImages[static_cast<int>(Images::IMAGE_DRONE_AND_CARGO)], 0.0, NULL, SDL_FLIP_NONE);
+		SDL_RenderCopyEx(gRenderer, gtextDrone.mTexture, NULL, &gImages[static_cast<int>(Images::IMAGE_TEXT_DRONE)], 0.0, NULL, SDL_FLIP_NONE);
+		SDL_RenderCopyEx(gRenderer, gTitle.mTexture, NULL, &gImages[static_cast<int>(Images::IMAGE_TITLE)], 0.0, NULL, SDL_FLIP_NONE);
+		SDL_RenderCopyEx(gRenderer, gtextDroneWithCargo.mTexture, NULL, &gImages[static_cast<int>(Images::IMAGE_TEXT_DRONE_AND_CARGO)], 0.0, NULL, SDL_FLIP_NONE);
+	}
+	else {
+		SDL_RenderCopyEx(gRenderer, gCSVfile.mTexture, NULL, &gImages[static_cast<int>(Images::IMAGE_CSV_FILE)], 0.0, NULL, SDL_FLIP_NONE);
+		SDL_RenderCopyEx(gRenderer, gKeyboard.mTexture, NULL, &gImages[static_cast<int>(Images::IMAGE_KEYBOARD)], 0.0, NULL, SDL_FLIP_NONE);
+		SDL_RenderCopyEx(gRenderer, gtextCSVfile.mTexture, NULL, &gImages[static_cast<int>(Images::IMAGE_TEXT_CSV_FILE)], 0.0, NULL, SDL_FLIP_NONE);
+		SDL_RenderCopyEx(gRenderer, gTitle.mTexture, NULL, &gImages[static_cast<int>(Images::IMAGE_TITLE)], 0.0, NULL, SDL_FLIP_NONE);
+		SDL_RenderCopyEx(gRenderer, gtextKeyboard.mTexture, NULL, &gImages[static_cast<int>(Images::IMAGE_TEXT_KEYBOARD)], 0.0, NULL, SDL_FLIP_NONE);
+	}
+	
+}
+
+SDL_Rect fillImagePosition(int x, int y, LTexture texture) {
+	SDL_Rect im;
+	im.x = x;
+	im.y = y;
+	im.w = texture.getWidth();
+	im.h = texture.getHeight();
+
+	return im;
 }
 
 bool init()
@@ -364,7 +424,7 @@ bool init()
 		}
 
 		//Create window
-		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH1, SCREEN_HEIGHT1, SDL_WINDOW_SHOWN);
 		if (gWindow == NULL)
 		{
 			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
@@ -404,28 +464,91 @@ bool loadMedia()
 	bool success = true;
 
 	//Load sprites
-	if (!gButtonSpriteSheetTexture.loadFromFile("drone.png"))
-	{
-		printf("Failed to load button sprite texture!\n");
-		success = false;
-	}
-	else
-	{
+	if (!gDrone.loadFromFile("drone.png")) printf("Failed to load button sprite texture!\n");
+	if (!gDroneWithCargo.loadFromFile("DroneAndCargo.png")) printf("Failed to load button sprite texture!\n");
+	if (!gKeyboard.loadFromFile("Keyboard.png")) printf("Failed to load button sprite texture!\n");
+	if (!gCSVfile.loadFromFile("CSV.png")) printf("Failed to load button sprite texture!\n");
+	if (!gtextDrone.loadFromFile("DroneChoice.png")) printf("Failed to load button sprite texture!\n");
+	if (!gtextDroneWithCargo.loadFromFile("DroneWithCargoChoice.png")) printf("Failed to load button sprite texture!\n");
+	if (!gTitle.loadFromFile("Title.png")) printf("Failed to load button sprite texture!\n");
+	if (!gtextKeyboard.loadFromFile("KeyboardInputChoice.png")) printf("Failed to load button sprite texture!\n");
+	if (!gtextCSVfile.loadFromFile("InputFileChoice.png")) printf("Failed to load button sprite texture!\n");
+	if (!gfileName.loadFromFile("FileName.png")) printf("Failed to load button sprite texture!\n");
+	//{
+		//printf("Failed to load button sprite texture!\n");
+		//success = false;
+	//}
+	//else
+	//{
 		//Set sprites
-		for (int i = 0; i < BUTTON_SPRITE_TOTAL; ++i)
+
+	gImages[static_cast<int>(Images::IMAGE_DRONE)] = fillImagePosition(60, 180, gDrone);
+	//gImages[static_cast<int>(Images::IMAGE_DRONE)].x = 60;
+	//gImages[static_cast<int>(Images::IMAGE_DRONE)].y = 180;
+	//gImages[static_cast<int>(Images::IMAGE_DRONE)].w = gDrone.getWidth();
+	//gImages[static_cast<int>(Images::IMAGE_DRONE)].h = gDrone.getHeight();
+
+	gImages[static_cast<int>(Images::IMAGE_DRONE_AND_CARGO)] = fillImagePosition(380, 180, gDroneWithCargo);
+	/*gImages[static_cast<int>(Images::IMAGE_DRONE_AND_CARGO)].x = 380;
+	gImages[static_cast<int>(Images::IMAGE_DRONE_AND_CARGO)].y = 180;
+	gImages[static_cast<int>(Images::IMAGE_DRONE_AND_CARGO)].w = gDroneWithCargo.getWidth();
+	gImages[static_cast<int>(Images::IMAGE_DRONE_AND_CARGO)].h = gDroneWithCargo.getHeight();*/
+
+	gImages[static_cast<int>(Images::IMAGE_TITLE)] = fillImagePosition(200, 10, gTitle);
+	//gImages[static_cast<int>(Images::IMAGE_TITLE)].x = 200;
+	//gImages[static_cast<int>(Images::IMAGE_TITLE)].y = 10;
+	//gImages[static_cast<int>(Images::IMAGE_TITLE)].w = gTitle.getWidth();
+	//gImages[static_cast<int>(Images::IMAGE_TITLE)].h = gTitle.getHeight();
+
+	gImages[static_cast<int>(Images::IMAGE_TEXT_DRONE)] = fillImagePosition(120, 120, gtextDrone);
+	/*gImages[static_cast<int>(Images::IMAGE_TEXT_DRONE)].x = 120;
+	gImages[static_cast<int>(Images::IMAGE_TEXT_DRONE)].y = 120;
+	gImages[static_cast<int>(Images::IMAGE_TEXT_DRONE)].w = gtextDrone.getWidth();
+	gImages[static_cast<int>(Images::IMAGE_TEXT_DRONE)].h = gtextDrone.getHeight();*/
+
+	gImages[static_cast<int>(Images::IMAGE_TEXT_DRONE_AND_CARGO)] = fillImagePosition(400, 120, gtextDroneWithCargo);
+	/*gImages[static_cast<int>(Images::IMAGE_TEXT_DRONE_AND_CARGO)].x = 400;
+	gImages[static_cast<int>(Images::IMAGE_TEXT_DRONE_AND_CARGO)].y = 120;
+	gImages[static_cast<int>(Images::IMAGE_TEXT_DRONE_AND_CARGO)].w = gtextDroneWithCargo.getWidth();
+	gImages[static_cast<int>(Images::IMAGE_TEXT_DRONE_AND_CARGO)].h = gtextDroneWithCargo.getHeight();*/
+
+	gImages[static_cast<int>(Images::IMAGE_CSV_FILE)] = fillImagePosition(60, 180, gCSVfile);
+	//gImages[static_cast<int>(Images::IMAGE_CSV_FILE)].x = 60;
+	//gImages[static_cast<int>(Images::IMAGE_CSV_FILE)].y = 180;
+	//gImages[static_cast<int>(Images::IMAGE_CSV_FILE)].w = gCSVfile.getWidth();
+	//gImages[static_cast<int>(Images::IMAGE_CSV_FILE)].h = gCSVfile.getHeight();
+
+	gImages[static_cast<int>(Images::IMAGE_KEYBOARD)] = fillImagePosition(380, 180, gKeyboard);
+	/*gImages[static_cast<int>(Images::IMAGE_KEYBOARD)].x = 380;
+	gImages[static_cast<int>(Images::IMAGE_KEYBOARD)].y = 180;
+	gImages[static_cast<int>(Images::IMAGE_KEYBOARD)].w = gKeyboard.getWidth();
+	gImages[static_cast<int>(Images::IMAGE_KEYBOARD)].h = gKeyboard.getHeight();*/
+
+	gImages[static_cast<int>(Images::IMAGE_TEXT_CSV_FILE)] = fillImagePosition(60, 120, gtextCSVfile);
+	/*gImages[static_cast<int>(Images::IMAGE_TEXT_CSV_FILE)].x = 60;
+	gImages[static_cast<int>(Images::IMAGE_TEXT_CSV_FILE)].y = 120;
+	gImages[static_cast<int>(Images::IMAGE_TEXT_CSV_FILE)].w = gtextCSVfile.getWidth();
+	gImages[static_cast<int>(Images::IMAGE_TEXT_CSV_FILE)].h = gtextCSVfile.getHeight();*/
+
+	gImages[static_cast<int>(Images::IMAGE_TEXT_KEYBOARD)] = fillImagePosition(450, 120, gtextKeyboard);
+	/*gImages[static_cast<int>(Images::IMAGE_TEXT_KEYBOARD)].x = 450;
+	gImages[static_cast<int>(Images::IMAGE_TEXT_KEYBOARD)].y = 120;
+	gImages[static_cast<int>(Images::IMAGE_TEXT_KEYBOARD)].w = gtextKeyboard.getWidth();
+	gImages[static_cast<int>(Images::IMAGE_TEXT_KEYBOARD)].h = gtextKeyboard.getHeight();*/
+
+
+		/*for (int i = 0; i < BUTTON_SPRITE_TOTAL; ++i)
 		{
-			gSpriteClips[i].x = 0;
-			gSpriteClips[i].y = i * 200;
-			gSpriteClips[i].w = BUTTON_WIDTH;
-			gSpriteClips[i].h = BUTTON_HEIGHT;
-		}
+			gImages [i].x = 0;
+			gImage[i].y = i * 200;
+			gImages[i].w = BUTTON_WIDTH;
+			gImages[i].h = BUTTON_HEIGHT;
+		}*/
 
 		//Set buttons in corners
-		gButtons[0].setPosition(0, 0);
-		gButtons[1].setPosition(SCREEN_WIDTH - BUTTON_WIDTH, 0);
-		gButtons[2].setPosition(0, SCREEN_HEIGHT - BUTTON_HEIGHT);
-		gButtons[3].setPosition(SCREEN_WIDTH - BUTTON_WIDTH, SCREEN_HEIGHT - BUTTON_HEIGHT);
-	}
+		gButtons[0].setPosition(60, 180);
+		gButtons[1].setPosition(380, 180);
+	//}
 
 	return success;
 }
@@ -433,7 +556,16 @@ bool loadMedia()
 void close()
 {
 	//Free loaded images
-	gButtonSpriteSheetTexture.free();
+	gDrone.free();
+	gDroneWithCargo.free();
+	gtextCSVfile.free();
+	gtextDrone.free();
+	gtextDroneWithCargo.free();
+	gtextKeyboard.free();
+	gKeyboard.free();
+	gCSVfile.free();
+	gTitle.free();
+	gfileName.free();
 
 	//Destroy window	
 	SDL_DestroyRenderer(gRenderer);
@@ -448,6 +580,7 @@ void close()
 
 int main(int argc, char* args[])
 {
+	bool choice = false;
 	//Start up SDL and create window
 	if (!init())
 	{
@@ -481,25 +614,48 @@ int main(int argc, char* args[])
 					}
 
 					//Handle button events
-					for (int i = 0; i < TOTAL_BUTTONS; ++i)
+					for (int i = 0; i < 2; ++i)
 					{
 						gButtons[i].handleEvent(&e);
 					}
 				}
+
+				if (gButtons[0].isPushed() && choice) {
+					std::cout << "first button pushed again\n";
+					quit = true;
+				}
+				else if (gButtons[1].isPushed() && choice) {
+					std::cout << "second button pushed again\n";
+					quit = true;
+				}
+				else if (gButtons[0].isPushed()) {
+					std::cout << "first button pushed\n";
+					gButtons[0].clearPushedFlag();
+					choice = true;
+				}
+				else if (gButtons[1].isPushed()) {
+					std::cout << "second button pushed\n";
+					gButtons[1].clearPushedFlag();
+					choice = true;
+				}
+
+				
 
 				//Clear screen
 				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 				SDL_RenderClear(gRenderer);
 
 				//Render buttons
-				for (int i = 0; i < TOTAL_BUTTONS; ++i)
+				render(choice);
+				/*for (int i = 0; i < TOTAL_BUTTONS; ++i)
 				{
 					gButtons[i].render();
-				}
+				}*/
 
 				//Update screen
 				SDL_RenderPresent(gRenderer);
 			}
+
 		}
 	}
 
