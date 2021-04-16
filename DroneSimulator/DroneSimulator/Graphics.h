2f54c6c1b78476304148ca45949dc7e9e433b5b3
Dionne Ariens <dlr.ariens@outlook.com>
2021-04-16 21:33:22 +0200

    Final project working!!

--- DroneSimulator/DroneSimulator/Graphics.h
+++ DroneSimulator/DroneSimulator/Graphics.h
@@ -9,20 +9,17 @@
 #include <algorithm>
 #include <vector>
 
-const int SCREEN_WIDTH = 1280;
-const int SCREEN_HEIGHT = 720;
-const int METERS_TO_PIXELS = 10;
-const int DRONE_CENTER_W = 103;
-const int DRONE_CENTER_H = 53;
-const int CARGO_CENTER_W = 41;
-const int CARGO_CENTER_H = 32;
-const int CARGO_CENTER_TO_DRONE_H = DRONE_CENTER_H + static_cast<int>(3 * DRONE_CENTER_H);
-const int CARGO_SIDE_TO_DRONE_H = CARGO_CENTER_H - 24;
+const int SCREEN_WIDTH = 1500;
+const int SCREEN_HEIGHT = 800;
+const int METERS_TO_PIXELS = 50;
+const int DRONE_CENTER_W = 51;
+const int DRONE_CENTER_H = 26;
+const int CARGO_CENTER_W = 20;
+const int CARGO_CENTER_H = 16;
 
-const int INIT_DRONE_W = (SCREEN_WIDTH / 2) - DRONE_CENTER_W;
-const int INIT_DRONE_H = (SCREEN_HEIGHT / 2) + DRONE_CENTER_H;
-const int INIT_CARGO_W = (SCREEN_WIDTH / 2) - CARGO_CENTER_W;
-const int INIT_CARGO_H = (SCREEN_HEIGHT / 2) + CARGO_CENTER_TO_DRONE_H;
+const int DRONE_OFFSET_W = (SCREEN_WIDTH / 2) - DRONE_CENTER_W;
+const int CARGO_AND_DRONE_OFFSET_H = (SCREEN_HEIGHT / 2) + DRONE_CENTER_H;
+const int CARGO_OFFSET_W = (DRONE_CENTER_W - CARGO_CENTER_W) + DRONE_OFFSET_W;
 
 class Graphics
 {
@@ -51,7 +48,8 @@ public:
 		if (SDL_Init(SDL_INIT_VIDEO) < 0){ printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError()); return false;}
 		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) printf("Warning: Linear texture filtering not enabled!");
 
-		gWindow = SDL_CreateWindow("Drone and Cargo game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		//gWindow = SDL_CreateWindow("Drone and Cargo game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		gWindow = SDL_CreateWindow("Drone and Cargo game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 		if (gWindow == NULL) { printf("Window could not be created! SDL Error: %s\n", SDL_GetError()); return false; }
 		
 		gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
@@ -68,40 +66,43 @@ public:
 		return loadFromFileHelper();
 	}
 
-	void render(int x, int y, bool cargo, SDL_Rect* clip = NULL, double angle = 0.0, int cargox = 0, int cargoy = 0, double cargoAngle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE) {
-		SDL_Rect rect = { x, y, droneWidth, droneHeight };
-		SDL_RenderCopyEx(gRenderer, droneTexture, clip, &rect, -angle, center, flip);
+	void render(int xDrone, int yDrone, bool cargo, double angle = 0.0, int xCargo = 0, int yCargo = 0, double cargoAngle = 0.0) {
+		SDL_Rect rect = { xDrone, yDrone, droneWidth/2, droneHeight/2 };
+		SDL_RenderCopyEx(gRenderer, droneTexture, NULL, &rect, -angle, NULL, SDL_FLIP_NONE);
 
 
 		if (cargo) {
-			SDL_Rect rect = { cargox, cargoy, cargoWidth, cargoHeight };
-			SDL_RenderCopyEx(gRenderer, cargoTexture, clip, &rect, -cargoAngle, center, flip);
+			SDL_Rect rect = { xCargo, yCargo, cargoWidth/2, cargoHeight/2 };
+			SDL_RenderCopyEx(gRenderer, cargoTexture, NULL, &rect, -cargoAngle, NULL, SDL_FLIP_NONE);
 
 			SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 255);
-			SDL_RenderDrawLine(gRenderer, (x + DRONE_CENTER_W), (y + DRONE_CENTER_H), (cargox + CARGO_CENTER_W), (cargoy + CARGO_SIDE_TO_DRONE_H));
+			SDL_RenderDrawLine(gRenderer, (xDrone + DRONE_CENTER_W), (yDrone + DRONE_CENTER_H), (xCargo + CARGO_CENTER_W), (yCargo + CARGO_CENTER_H));
 		}
 	}
 
 	void updateGraphics(std::vector<double> xk) {
-		double degrees = xk.at(2);
+		double droneAngle = xk.at(2);
 		int pxDrone = static_cast<int>(xk.at(0) * METERS_TO_PIXELS);
 		int pyDrone = static_cast<int>(-xk.at(1) * METERS_TO_PIXELS);
 
-		double cargoDegrees = 0.0;
+		double cargoAngle = 0.0;
 		int pxCargo = 0;
 		int pyCargo = 0;
 
 		bool cargo = false;
 
 		if (xk.size() == 9) {
+			cargoAngle = 180 * std::atan2(xk[5] - xk[0], xk[1] - xk[6]) / M_PI;
 			pxCargo = static_cast<int>(xk.at(5) * METERS_TO_PIXELS);
 			pyCargo = static_cast<int>(-xk.at(6) * METERS_TO_PIXELS);
+
 			cargo = true;
 		}
 		
 		SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 		SDL_RenderClear(gRenderer);
-		render(INIT_DRONE_W + pxDrone, INIT_DRONE_H + pyDrone, cargo, NULL, degrees, INIT_CARGO_W + pxCargo, INIT_CARGO_H + pyCargo, cargoDegrees);
+		render(DRONE_OFFSET_W + pxDrone, CARGO_AND_DRONE_OFFSET_H + pyDrone, cargo, droneAngle, CARGO_OFFSET_W + pxCargo, CARGO_AND_DRONE_OFFSET_H + pyCargo, cargoAngle);
+		//render(pxDrone, pyDrone, cargo, NULL, degrees, pxCargo, pyCargo, cargoDegrees);
 		
 		SDL_RenderPresent(gRenderer);
 	}
